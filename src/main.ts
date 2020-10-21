b6e8fd9e171aaff3f2f67307025ec880f3e1bce0
caixiaomao <532001566@qq.com>
2020-10-21 11:52:52 +0800

    引入 ant-design-vue

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+// ant-design-vue
+import antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(antd)
   .mount("#app");
